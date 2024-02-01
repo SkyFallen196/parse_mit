afc8b525b457325b150312530615876800f3772b
SkyFallen196 <81759161+SkyFallen196@users.noreply.github.com>
2024-02-02 00:24:30 +0500

    parse and convert to csv (ALL)

--- all_page_topic_parse.py
+++ all_page_topic_parse.py
@@ -2,31 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-base_url = 'https://news.mit.edu/topic/machine-learning?page='
-page_num = 0
-urls = []
+topics = ['machine-learning', 'social-justice', 'startups', 'black-holes', 'classes-and-programs']
+base_url = 'https://news.mit.edu/topic/'
 
-while True:
-    url = base_url + str(page_num)
-    response = requests.get(url).text
-    data = BeautifulSoup(response, 'html.parser')
-    topic = data.find('h1').text
-    titles = data.find_all('h3', class_='term-page--news-article--item--title')
-    descriptions = data.find_all('p', class_='term-page--news-article--item--dek')
-    dates = data.find_all('p', class_='term-page--news-article--item--publication-date')
+all_urls = []
 
-    if len(titles) == 0:
-        break
+for topic in topics:
+    page_num = 0
+    urls = []
+    while True:
+        url = base_url + topic + '?page=' + str(page_num)
+        response = requests.get(url).text
+        data = BeautifulSoup(response, 'html.parser')
+        titles = data.find_all('h3', class_='term-page--news-article--item--title')
+        descriptions = data.find_all('p', class_='term-page--news-article--item--dek')
+        dates = data.find_all('p', class_='term-page--news-article--item--publication-date')
 
-    for title, description, date in zip(titles, descriptions, dates):
-        urls.append({
-            'Topic': topic,
-            'Title': title.a.span.text,
-            'Description': description.span.text,
-            'Date': date.time.text
-        })
+        if len(titles) == 0:
+            break
 
-    page_num += 1
+        for title, description, date in zip(titles, descriptions, dates):
+            urls.append({
+                'Topic': topic.replace('-', ' ').title(),
+                'Title': title.a.span.text,
+                'Description': description.span.text,
+                'Date': date.time.text
+            })
 
-    df = pd.DataFrame(urls)
-    df.to_csv('machine_learning_news.csv', index=False)
+        page_num += 1
+
+    all_urls.extend(urls)
+
+df = pd.DataFrame(all_urls)
+df.to_csv('mit_news_articles.csv', index=False)
